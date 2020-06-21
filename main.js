02937f11bbf6ec1d7798d05436b40d5eeba3d96c
Tham53 <59705897+Tham53@users.noreply.github.com>
2020-06-21 18:00:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -56,3 +56,67 @@ function allUsers() {
     })
     document.getElementById("allContacts").innerHTML = " ";
 }
+
+
+//Project 1: Adding Unit Test to your Fetch Requests
+//Last week you paired-up and built an app that fetches data from an API.
+//Today you're going to add unit test for multiple situations:
+
+//Miss typed address
+//Out of range request
+//Wrong api key
+//Hypothetical 4
+//Hypothetical 5
+//Unit Test Instructions
+//Move to be able to pair program with your partner
+//git status to make sure you each have pushed up and merge all of your changes
+//Work on one computer and add the 5 unit tests mentioned about. You will have to refactor
+//your request a bit to added in fetch as a dependency.
+
+
+
+
+
+
+
+describe("missTypedaddress", () => {
+  it("wrong address", () => {
+    const testFetch = url => {
+      assert(url === "https://randomuser.me/api/?results=5");
+      return new Promise(function() {});
+    };
+    missTypedaddress(testFetch, "wrong address");
+  });
+  describe("outofRangerequest", () => {
+    it("request is out of range", () => {
+      const testFetch = url => {
+        assert(url === "https://randomuser.me/api/?results=5");
+        return new Promise(function() {});
+      };
+      outofRangerequest(testFetch, "request is out of range");
+    });
+    describe("wrongAPIkey", () => {
+      it("wrong API key", () => {
+        const testFetch = url => {
+          assert(url === "https://randomuser.me/api/?results=5");
+          return new Promise(function() {});
+        };
+        wrongAPIkey(testFetch, "wrong API key");
+      });
+      describe("getRandomUser", () => {
+        it("tests fetch and random users endpoint", () => {
+          const testFetch = url => {
+            assert(url === "https://randomuser.me/api/?results=5");
+            return new Promise(function() {});
+          };
+          getRandomUser(testFetch, "random user");
+        });
+        describe("test5users", () => {
+  it("tests 5 random users", () => {
+    const testFetch = url => {
+      assert(url === "https://randomuser.me/api/?results=5");
+      return new Promise(function() {});
+    };
+    test5usersr(testFetch, "?results=5");
+  });
+});
